59dfe2e052f6502e313de7cd5ef3d0c3ac15d59a
Matt Dominey <matt@dominey.io>
2021-04-25 18:52:57 -0400

    Restructure terraform files

---
+++ aws/variables.tf
@@ -0,0 +1,9 @@
+variable "docker_hub_username" {
+  type = string
+  description = "Username to push rubian images through"
+}
+
+variable "docker_hub_password" {
+  type = string
+  description = "Docker Hub password"
+}
